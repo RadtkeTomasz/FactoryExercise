bccd28651ae622ed23264b46e1cebdf8f0099085
TomaszR <tomeas23@email.com>
2023-06-02 14:13:51 +0200

    Prowizje info

--- FactoryExercise/PaymentApiHelper.cs
+++ FactoryExercise/PaymentApiHelper.cs
@@ -12,8 +12,8 @@ public class PaymentApiHelper : IPayment
         
         switch (provider)
         {
-            case PaymentServiceProvider.Bank1: return "Bank1API pobrało ";
-            case PaymentServiceProvider.Bank2: return "Bank2API";
+            case PaymentServiceProvider.Bank1: return "Bank1API pobrało prowizję 2% dla zakupów poniżej 50zl, 1.5% dla zakupów powyżej";
+            case PaymentServiceProvider.Bank2: return "Bank2API pobrano 1.5procent prowizji";
             case PaymentServiceProvider.BLIK: return "BLIKAPI";
             case PaymentServiceProvider.Przelewy24: return "Przelewy24API";
             default: throw new NotImplementedException("Payment provider not found");
