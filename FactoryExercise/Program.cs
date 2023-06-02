b2be47c2bbeb5d791f932114668272a562de8a09
TomaszR <tomeas23@email.com>
2023-06-02 14:19:47 +0200

    Products list

--- FactoryExercise/Program.cs
+++ FactoryExercise/Program.cs
@@ -3,7 +3,7 @@
 var ProductsInBasket = new List<Product>()
 { new Product { Description = "A",Name = "ProductA", Price = 20.00M },
   new Product { Description = "B", Name = "ProductB", Price = 50.00M},
- new Product { Description = "B", Name = "ProductB", Price = 50.00M }};
+ new Product { Description = "C", Name = "ProductC", Price = 40.00M }};
 
 var payment = PaymentFactory.GeneratePayment(PaymentServiceProvider.Bank1, "SomeDescription", ProductsInBasket.Sum(p => p.Price));
 var apiHelper = new PaymentApiHelper();
